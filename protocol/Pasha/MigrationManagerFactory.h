b787b8fcf577e19ee491be9b76be9eb92bb5911f
Temitayo Awosemo <temia@utexas.edu>
2026-05-04 17:08:59 -0500

    originial auto

--- protocol/Pasha/MigrationManagerFactory.h
+++ protocol/Pasha/MigrationManagerFactory.h
@@ -14,6 +14,7 @@
 #include "protocol/Pasha/PolicyClock.h"
 #include "protocol/Pasha/PolicyAging.h"
 #include "protocol/Pasha/PolicyAgingScanAware.h"
+#include "protocol/Pasha/PolicyAgingAutoScan.h"
 #include "protocol/Pasha/PolicyWorkloadAdaptive.h"
 #include "protocol/Pasha/PolicyAdaptiveAging.h"
 
@@ -103,6 +104,15 @@ class MigrationManagerFactory {
                                         partition_num,
                                         when_to_move_out,
                                         hw_cc_budget);
+                        } else if (migration_policy == "AgingAutoScan") {
+                                migration_manager = new PolicyAgingAutoScan(
+                                        std::bind(&SundialPashaHelper::move_from_partition_to_shared_region, sundial_pasha_global_helper, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5),
+                                        std::bind(&SundialPashaHelper::move_from_shared_region_to_partition, sundial_pasha_global_helper, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3),
+                                        std::bind(&SundialPashaHelper::delete_and_update_next_key_info, sundial_pasha_global_helper, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5),
+                                        coordinator_id,
+                                        partition_num,
+                                        when_to_move_out,
+                                        hw_cc_budget);
                         } else {
                                 CHECK(0);
                         }
@@ -173,6 +183,15 @@ class MigrationManagerFactory {
                                         hw_cc_budget);
                         } else if (migration_policy == "AgingScanAware") {
                                 migration_manager = new PolicyAgingScanAware(
+                                        std::bind(&TwoPLPashaHelper::move_from_partition_to_shared_region, twopl_pasha_global_helper, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5),
+                                        std::bind(&TwoPLPashaHelper::move_from_shared_region_to_partition, twopl_pasha_global_helper, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3),
+                                        std::bind(&TwoPLPashaHelper::delete_and_update_next_key_info, twopl_pasha_global_helper, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5),
+                                        coordinator_id,
+                                        partition_num,
+                                        when_to_move_out,
+                                        hw_cc_budget);
+                        } else if (migration_policy == "AgingAutoScan") {
+                                migration_manager = new PolicyAgingAutoScan(
                                         std::bind(&TwoPLPashaHelper::move_from_partition_to_shared_region, twopl_pasha_global_helper, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5),
                                         std::bind(&TwoPLPashaHelper::move_from_shared_region_to_partition, twopl_pasha_global_helper, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3),
                                         std::bind(&TwoPLPashaHelper::delete_and_update_next_key_info, twopl_pasha_global_helper, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5),
